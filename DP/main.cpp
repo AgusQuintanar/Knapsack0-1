0f0d3c4eb9414514cca7f534d51e821a2f4ea385
Jonathan <jonathanchatab@hotmail.com>
2020-04-24 21:44:27 -0500

    faltan pruebas pero funcionan ambos nw y w

--- DP/main.cpp
+++ DP/main.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 typedef int ulli;
 
+
 ulli max(ulli a, ulli b){
+	//max, recibe dos numeros, retorna el mayor.
 	return a >= b ? a : b;
 }
 
@@ -16,27 +18,109 @@ void solveA(ulli n, vector<ulli> &b, vector<ulli> &w, ulli k){
 	//puede lograr una mochila con cierta capacidad tomando máximo 1 elemento.
 	
 	vector<vector<ulli> > dp = vector<vector<ulli> >(n+1, vector<ulli>(k+1));
-	for(ulli i=0; i<k+1; i++){
-		dp[0][i] = 0;
-	}
-	for(ulli i=0; i<n+1; i++){
-		dp[i][0] = 0;
-	}
-		
+
+	//Relacion de recurrencia	
 	for(ulli i=1; i<n+1; i++){
 		for(ulli j=1; j<k+1; j++){
-			dp[i][j] = max(dp[i-1][j], j-w[i-1] < 0 ? 0 : dp[i-1][j-w[i-1]] + b[i-1]);		
+			if(j-w[i-1] < 0){ // Si no cabe
+				dp[i][j] = dp[i-1][j]; //Toma maximo anterior
+			}
+			else{ // Si cabe
+				if(dp[i-1][j] < dp[i-1][j-w[i-1]] + b[i-1]){
+					dp[i][j] = dp[i-1][j-w[i-1]] + b[i-1]; // Es mejor agarrar el actual y llenar lo que resta con lo mejor
+				}
+				else dp[i][j] = dp[i-1][j]; // El anterior es mejor que agarrar el nuevo y lo que restaba
+			}	
 		}
 	}
-		
+
+	//Impresion del beneficio máximo
+	cout<<dp[n][k]<<'\n';
+
+	//Impresion de elementos usados
+	#ifdef ELEMENTS
+	for(int i=n, j=k; i>= 1; i--){
+		if(j<1) break;
+		if(dp[i][j] != dp[i-1][j]){ // Si diferente al de arriba, eligio el elemento
+			j -= w[i-1];
+			cout<<i<<" ";
+		}
+	}
+	cout<<'\n';
+	#endif	
+
+	//Impresion de matriz
+	#ifdef MATRIX
 	for(ulli i=0; i<n+1; i++){
 		for(ulli j=0; j<k+1; j++){
 			cout<<dp[i][j]<<" ";
 		}
 		cout<<'\n';
 	}
+	#endif
+}
 
-	cout<<dp[n][k]<<endl;
+
+void solveB(ulli n, vector<ulli> &b, vector<ulli> &w, ulli k){
+    vector<vector<int> > dp(2, vector<int>(k+1));
+  
+    // iterate through all items 
+    int i = 0; 
+    while (i < n) // one by one traverse each element 
+    { 
+        int j = 0; // traverse all weights j <= W 
+  
+        // if i is odd that mean till now we have odd 
+        // number of elements so we store result in 1th 
+        // indexed row 
+        if (i%2!=0) 
+        { 
+            while (++j <= k) // check for each value 
+            {
+                if (w[i] <= j){ // include element
+			if(b[i] + dp[0][j-w[i]] > dp[0][j]){
+				dp[1][j] = b[i] + dp[0][j-w[i]];
+			}
+			else dp[1][j] = dp[0][j];
+			
+		}
+                else           // exclude element 
+                    	dp[1][j] = dp[0][j]; 
+            } 
+  
+        } 
+  
+        // if i is even that mean till now we have even number 
+        // of elements so we store result in 0th indexed row 
+        else
+        { 
+            while(++j <= k) 
+            { 
+                if (w[i] <= j){
+			if(b[i] + dp[1][j-w[i]] > dp[1][j]){
+				dp[0][j] = b[i] + dp[1][j-w[i]];
+			}
+			else dp[0][j] = dp[1][j];
+		}
+                else
+                    dp[0][j] = dp[1][j]; 
+            } 
+        } 
+        i++; 
+    } 
+  
+    // Return mat[0][W] if n is odd, else mat[1][W] 
+    cout<<( (n%2 != 0)? dp[0][k] : dp[1][k])<<'\n'; 
+    
+	#ifdef MATRIX
+	for(ulli i=0; i<2; i++){
+		for(ulli j=0; j<k+1; j++){
+			cout<<dp[i][j]<<" ";
+		}
+		cout<<'\n';
+	}
+	#endif
+
 }
 
 int main(){
@@ -53,6 +137,5 @@ int main(){
 	ulli k; // Capacidad de mochila
 	cin>>k;
 
-	solveA(n, b, w, k);
-
+	solveB(n, b, w, k);
 }
